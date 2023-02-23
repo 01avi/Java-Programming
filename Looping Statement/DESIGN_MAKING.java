3e5f9ed38cfe1efc5e8d036c412d12668a93ba86
Aviral <110219413+01avi@users.noreply.github.com>
2023-02-23 19:14:44 +0530

    Design

---
+++ Looping Statement/DESIGN_MAKING.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class DESIGN_MAKING
+{
+public static void main(String[] args){
+   Scanner input=new Scanner(System.in);
+    System.out.println("ENTER NUMBER FOR DESIGN:- ");
+    int n=input.nextInt();
+    int i,j;
+    for(i=0;i<=n;i++){
+        for(j=0;j<=i;j++){
+            System.out.println("*");
+        }
+        System.out.println("\n");
+     }
+
+    }
+}
