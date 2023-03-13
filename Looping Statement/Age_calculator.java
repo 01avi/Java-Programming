4fc2825a096ddb0af96cb52b8e8ffcc5362445fb
Aviral <110219413+01avi@users.noreply.github.com>
2023-03-13 19:51:29 +0530

    java

---
+++ Looping Statement/Age_calculator.java
@@ -0,0 +1,15 @@
+ import java.util.Scanner;
+ public class Age_calculator
+ {
+ public static void main(String[] args){
+     int b;
+     do{
+     Scanner input=new Scanner(System.in);
+     System.out.println("ENTER YOUR BIRTH YEAR:-");
+     int a = input.nextInt();
+     b=2022-a;
+    System.out.println("YOUR AGE IN 2022 IS:-"+b);
+    System.out.println("\n");
+ } while(b!=0);
+}
+}
